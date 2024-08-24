20f024f1412b35aa6449123dfbee7c9ee7a9defa
AngieJoya99 <angie.joya@correounivalle.edu.co>
2024-08-24 14:56:06 -0500

    Update ejercicios-taller1.rkt

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -8,9 +8,7 @@ Emily Nuñez - 2240156|#
 #|Punto 1
 invert: List x predicado -> List
 usage: (invert lista p) = Lista con pares invertidos (y,x) que cumplen el predicado p 
-
-<lista>:= ()
-          ((<entero> <entero>) <lista>)|#
+|#
 
 (define invert
     (lambda (lista p)
@@ -20,7 +18,7 @@ usage: (invert lista p) = Lista con pares invertidos (y,x) que cumplen el predic
             [(null? (cdr lista)) 
                 (if (and (p (caar lista)) (p (cadr (car lista)))) 
                     (cons (cons (cadr (car lista)) (cons (caar lista) '())) '())
-                    lista
+                    '()
                 )                
             ]
 
@@ -34,6 +32,13 @@ usage: (invert lista p) = Lista con pares invertidos (y,x) que cumplen el predic
     )
 )
 
+#|Casos de prueba:
 (invert '((3 2) (4 2) (1 5) (2 8)) even?)
+|#
 
 
+#|---------------------------------------------------------------------------------
+Punto 2
+down: List -> List
+usage: (down lista) = Lista con cada elemento en un nivel más de paréntesis
+|#
