9b37e7e913ca6cc811f3aec69c74c2e309be708e
AngieJoya99 <angie.joya@correounivalle.edu.co>
2024-08-24 11:13:36 -0500

    Crear proyecto

---
+++ ejercicios-taller1.rkt
@@ -0,0 +1,11 @@
+#|Angie Joya - 2322609
+Emily Nuñez - 2240156|#
+
+#lang eopl
+
+#|Documentar procedimientos, gramáticas, funciones auxiliares, casos de prueba (min 2)|#
+
+#|Punto 1
+invert: List x predicado -> List
+usage: (invert lista p) = Lista con pares invertidos (y,x) que cumplen el predicado p 
+Propósito: |#
