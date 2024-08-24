5696932d74a109834334e39c417e4559abcbe8c7
AngieJoya99 <angie.joya@correounivalle.edu.co>
2024-08-24 14:49:57 -0500

    Update ejercicios-taller1.rkt

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -8,4 +8,32 @@ Emily Nuñez - 2240156|#
 #|Punto 1
 invert: List x predicado -> List
 usage: (invert lista p) = Lista con pares invertidos (y,x) que cumplen el predicado p 
-Propósito: |#
+
+<lista>:= ()
+          ((<entero> <entero>) <lista>)|#
+
+(define invert
+    (lambda (lista p)
+        (cond
+            [(null? lista) lista]
+
+            [(null? (cdr lista)) 
+                (if (and (p (caar lista)) (p (cadr (car lista)))) 
+                    (cons (cons (cadr (car lista)) (cons (caar lista) '())) '())
+                    lista
+                )                
+            ]
+
+            [(not(null? (cdr lista)))
+                (if (and (p (caar lista)) (p (cadr (car lista)))) 
+                    (cons (cons (cadr (car lista)) (cons (caar lista) '())) (invert (cdr lista) p))
+                    (cons '() (invert (cdr lista) p))
+                )                
+            ]             
+        )
+    )
+)
+
+(invert '((3 2) (4 2) (1 5) (2 8)) even?)
+
+
